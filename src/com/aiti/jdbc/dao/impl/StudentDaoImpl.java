5fd80e08c55f6ceb3416a3b95b626f2deaeb50e6
fanhengyuan <fanhengyuan1994@gmail.com>
2018-05-30 20:17:48 +0800

    抽取出DQL执行方法

--- src/com/aiti/jdbc/dao/impl/StudentDaoImpl.java
+++ src/com/aiti/jdbc/dao/impl/StudentDaoImpl.java
@@ -3,13 +3,7 @@ package com.aiti.jdbc.dao.impl;
 import com.aiti.jdbc.dao.IStudentDao;
 import com.aiti.jdbc.domain.Student;
 import com.aiti.jdbc.util.CRUDTemplate;
-import com.aiti.jdbc.util.JDBCUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDaoImpl implements IStudentDao {
@@ -33,62 +27,14 @@ public class StudentDaoImpl implements IStudentDao {
 
     @Override
     public Student get(int id) {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            conn = JDBCUtil.getConnection();
-
-            // 3.sql 语句
-            String sql = "select * from student where id =?;";
-            ps = conn.prepareStatement(sql);
-            ps.setInt(1, id);
-            // 4.执行 sql
-            rs = ps.executeQuery();
-            if(rs.next()) {
-                Student stu = new Student();
-                stu.setName(rs.getString("name"));
-                stu.setAge(rs.getInt("age"));
-                stu.setId(rs.getInt("id"));
-                return stu;
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            JDBCUtil.close(conn, ps, rs);
-        }
-        return null;
+        String sql = "select * from student where id =?;";
+        List<Student>  list = CRUDTemplate.executeQuery(sql, id);
+        return list.size() == 1 ? list.get(0) : null;
     }
 
     @Override
     public List<Student> getAll() {
-        Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
-        try {
-            conn = JDBCUtil.getConnection();
-
-            // 3.sql 语句
-            String sql = "select * from student;";
-            System.out.println(sql);
-            st = conn.createStatement();
-            // 4.执行 sql
-            rs = st.executeQuery(sql);
-
-            List<Student> listStu = new ArrayList<>();
-            while(rs.next()) {
-                Student stu = new Student();
-                stu.setName(rs.getString("name"));
-                stu.setAge(rs.getInt("age"));
-                stu.setId(rs.getInt("id"));
-                listStu.add(stu);
-            }
-            return listStu;
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            JDBCUtil.close(conn, st, rs);
-        }
-        return null;
+        String sql = "select * from student;";
+        return CRUDTemplate.executeQuery(sql);
     }
 }
--- src/com/aiti/jdbc/util/CRUDTemplate.java
+++ src/com/aiti/jdbc/util/CRUDTemplate.java
@@ -1,7 +1,12 @@
 package com.aiti.jdbc.util;
 
+import com.aiti.jdbc.domain.Student;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CRUDTemplate {
     public static int executeUpdate(String sql, Object...params) {
@@ -26,5 +31,36 @@ public class CRUDTemplate {
             JDBCUtil.close(conn, ps, null);
         }
         return 0;
+    }
+
+    public static List<Student> executeQuery(String sql, Object... params) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        // 集合存储学生信息
+        List<Student> list = new ArrayList<>();
+        try {
+            conn = JDBCUtil.getConnection();
+            ps = conn.prepareStatement(sql);
+
+            for(int i = 0; i < params.length; i++) {
+                ps.setObject(i + 1, params[i]);
+            }
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Student stu = new Student();
+                stu.setName(rs.getString("name"));
+                stu.setAge(rs.getInt("age"));
+                stu.setId(rs.getInt("id"));
+                list.add(stu);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
